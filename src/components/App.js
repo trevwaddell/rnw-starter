6398eade60e1e6842ced17c01aba8d916329a2b9
Trevor Waddell <trevorwaddell@Trevors-MBP.attlocal.net>
2018-11-04 03:45:52 -0500

    got latest rnw and compat react, react-dom

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>React Native Starter</Text>
+        <Text>React Native Web Starter</Text>
       </View>
     );
   }
